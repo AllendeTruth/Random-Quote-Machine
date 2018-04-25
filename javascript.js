7c9150ce463bf2d6c9da31d5d83dad972743b50a
AllendeTruth <nandersizzle@gmail.com>
2018-04-25 10:31:01 -0500

    Random Quote machine, basic html, and css in place, working on JS logic first commmit

---
+++ javascript.js
@@ -0,0 +1,20 @@
+// alert("connected");
+
+//connect api
+var api = "https://api.whatdoestrumpthink.com/api/v1/quotes/random"
+//function that accesses api and connects to event listeners
+function(getQuote){
+    // fetch(api)
+    console.log("newquote button was clicked");
+}
+// select id's   .button__newquote  .quotebox__content #quote-source
+function displayQuote(quote){
+    var quotetext = document.querySelector(".quotebox__content");
+    quotetext.textContent = quote;
+}
+var newQuoteButton= document.querySelector(".button__newquote");
+newQuoteButton.addEventListener('click', getQuote);
+//add event listener
+
+
+//display text
