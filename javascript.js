ea4c5256796f5cac3707d35cdf547e2788af0f8c
AllendeTruth <nandersizzle@gmail.com>
2018-04-25 10:37:43 -0500

    added fetch(api) and started function call for response

--- javascript.js
+++ javascript.js
@@ -4,8 +4,13 @@
 var api = "https://api.whatdoestrumpthink.com/api/v1/quotes/random";
 //function that accesses api and connects to event listeners
 function getQuote(){
-    // fetch(api)
-    console.log("newquote button was clicked");
+    // fetch gets URL;
+    fetch(api)
+        // .then() converts resons to json
+        .then(function(response){
+            response.json();
+        })
+    // console.log("newquote button was clicked");
 };
 // select id's   .button__newquote  .quotebox__content #quote-source
 function displayQuote(quote){
