e802bdffa7fc13041bf7422f53731af2a532ed49
AllendeTruth <nandersizzle@gmail.com>
2018-04-25 10:34:27 -0500

    changed function (getQuote) to function getQuote() click listener works now

--- javascript.js
+++ javascript.js
@@ -1,12 +1,12 @@
 // alert("connected");
 
 //connect api
-var api = "https://api.whatdoestrumpthink.com/api/v1/quotes/random"
+var api = "https://api.whatdoestrumpthink.com/api/v1/quotes/random";
 //function that accesses api and connects to event listeners
-function(getQuote){
+function getQuote(){
     // fetch(api)
     console.log("newquote button was clicked");
-}
+};
 // select id's   .button__newquote  .quotebox__content #quote-source
 function displayQuote(quote){
     var quotetext = document.querySelector(".quotebox__content");
